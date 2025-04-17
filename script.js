c0a853b32c91b327d33f895bfe8ff84d84a2751b
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-17 13:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.getElementsByName("fname")[0].value.trim()
+	let lastName = document.getElementsByName("lname")[0].value.trim()
+	console.log(firstName)
+	console.log(lastName)
+	alert(`${firstName} ${lastName}` );
 
 }
